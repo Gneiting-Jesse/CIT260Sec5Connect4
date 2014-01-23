f4fd7ee71077e08421bc26de015a88b35bd1def4
Gneiting-Jesse <gne99003@byui.edu>
2014-01-23 11:02:59 -0700

    Adding getName and displayHelp function. Also modified the main class with addition of two instance variables String name and String instructions.

--- src/connect4/Connect4.java
+++ src/connect4/Connect4.java
@@ -6,17 +6,37 @@
 
 package connect4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Owner
  */
 public class Connect4 {
 
-    /**
-     * @param args the command line arguments
-     */
+    // Instance variables
+    String name;
+    String instructions = "This is a game of Connect4.\n\n"
+            + "You will have the option of choosing a 1 player game or\n"
+            + "a 2 player game. Each player will have a different colored\n"
+            + "circular disc with the object of the game to place 4 of your\n"
+            + "discs consecutively in a line either vertically,\n "
+            + "horizontally or diagonally.\n\n"
+            + "Good Luck!\n\n";
     public static void main(String[] args) {
+        Connect4 myGame = new Connect4();
+        myGame.getName();
+        myGame.displayHelp();
         // TODO code application logic here
     }
-    
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+        
+    }
+    public void displayHelp () {
+        System.out.println("\nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
+    }
 }
