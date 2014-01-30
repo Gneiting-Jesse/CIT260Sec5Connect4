8e4a4bdd817f7314e59756c9bc50f7e091f9f8d6
Gneiting-Jesse <gne99003@byui.edu>
2014-01-29 22:43:43 -0700

    Completed Lesson 3 Paired Programming - Part 2

--- src/connect4/PlayerStats.java
+++ src/connect4/PlayerStats.java
@@ -6,37 +6,44 @@
 
 package connect4;
 
+import java.util.Scanner;
+
 /**
  *
- * @author marcy
+ * @author jesse
  */
 public class PlayerStats {
-    int wins;
-    int losses;
-    int percentage;
+    long wins;
+    long losses;
+    int totalGamesPlayed;
+    float percentage;
 
-public static void main(Strings[] args) {
+
+    public static void main(String[] args) {
     PlayerStats winStats = new PlayerStats();
-    winStats.displayWins();
-}
+    winStats.getStats();
+    }
 
-public void displayWins(){
-    wins = 3;
-    losses = 7;
-int totalWins = this.wins - this.losses;
 
-System.out.println("The total wins is " + totalWins);
-}
-
-public void displayPercentage() {
-percentage = (double) (this.wins/(this.wins + this.losses));
-
-System.out.println("The percentage of wins is " + percentage);
-}
-
-    private static class Strings {
-
-        public Strings() {
+    public void getStats(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter Number of Wins: ");
+        this.wins = reader.nextInt();
+        if (this.wins < 0) {
+            System.out.println("Invalid number of wins \n");
         }
+        System.out.println("Enter Number of Losses: ");
+        this.losses = reader.nextInt();
+        if (this.losses < 0) {
+            System.out.println("Invalid number of Losses \n");
+        }
+        totalGamesPlayed = (int) (this.wins + this.losses);
+        System.out.println("The total games played is " + totalGamesPlayed);
+        if (this.totalGamesPlayed < 0) {
+            System.out.println("Invalid number of Games Played \n");
+        }
+        percentage = (this.wins * 100/ this.totalGamesPlayed);
+            System.out.println("\"Wow\" Your winning percentage is " + percentage + "%"
+                + "\nThat is a great job!!!");
     }
 }
