082f0047352e7b3b435095a2789b22c33164a605
marcy <marcy@marcy-PC>
2014-02-04 13:41:57 -0700

    individual programming assignment (Marcy)

---
+++ src/connect4/HelpMenuControl.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connect4;
+
+/**
+ *
+ * @author marcy
+ */
+public class HelpMenuControl  {
+    
+    public HelpMenuControl() {
+        
+    } 
+
+    public void displayGameHelp() {
+         
+        System.out.println("Holw cow it's working!! (game)");
+
+    }
+      
+    public void display1PlayerHelp() {
+   
+        System.out.println("I have no idea what to write (1)"); 
+
+    }
+            
+    public void display2PlayerHelp() {
+    
+        System.out.println("Uh...I don't know! (2)"); 
+    }
+    
+                   
+}
---
+++ src/connect4/HelpMenuView.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connect4;
+
+/**
+ *
+ * @author marcy
+ */
+import java.util.Scanner;
+
+public class HelpMenuView {
+        
+    private final static String[][] menuItems = {
+        {"G", "Game Objective"},
+        {"1", "1 Player Game"}, 
+        {"2", "2 Player Game"},      
+        {"Q", "Quit Help"}        
+    };
+    
+    // Create instance of the HelpMenuControl (action) class
+    private HelpMenuControl helpMenuControl = new HelpMenuControl();
+    
+    // default constructor
+    public HelpMenuView() {
+        
+    } 
+    public static void main(String[] args) {
+        HelpMenuView helpMenu = new HelpMenuView();
+        helpMenu.getInput();
+    }
+    // display the help menu and get the end users input selection
+    public void getInput() {       
+              
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            
+            this.display(); // display the menu
+            
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "G":
+                    this.helpMenuControl.displayGameHelp();
+                    break;
+                case "1":
+                    this.helpMenuControl.display1PlayerHelp();
+                    break;
+                case "2":
+                    this.helpMenuControl.display2PlayerHelp();
+                case "Q": 
+                    break;
+                default: 
+                    new connect4error().displayError("Invalid choice. Please enter a valid option.");
+                    continue;
+            }
+        } while (!command.equals("Q"));  
+        
+         return;
+    }
+
+        // displays the help menu
+    public final void display() {
+        System.out.println("\tEnter the character associated with one of the following options:");
+
+        for (int i = 0; i < HelpMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+    }
+  
+}
+
