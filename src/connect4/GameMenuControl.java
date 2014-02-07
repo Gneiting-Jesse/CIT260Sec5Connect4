d32784639d13b6567d3cdb4e473c5c01c0bd1b35
marcy <marcy@marcy-PC>
2014-02-06 21:24:36 -0700

    Lesson 4 individual programming assignment - created gamemenuview/control classes

---
+++ src/connect4/GameMenuControl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connect4;
+
+/**
+ *
+ * @author marcy
+ */
+public class GameMenuControl {
+    
+    //default constructor
+    public GameMenuControl(){
+        
+    }
+    
+    public void displayNewGame() {
+        
+        System.out.println("\tstub. This option would allow the user\n"
+                           +"\tto play a new game.");
+        
+    }
+    
+    public void displayStatistics() {
+        
+        System.out.println("\tstub. This option would allow the user\n"
+                           + "\tto go to the Statistics page.");
+        
+    }
+    
+    public void displayHelpMenu() {
+        
+        System.out.println("\tstub. This option would allow the user\n"
+                           + "\tto go to the Help Menu.");
+    }
+    
+}
