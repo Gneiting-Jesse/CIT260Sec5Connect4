d1e1f7a5848efc6441b395c5061f71b65575c091
Gneiting-Jesse <gne99003@byui.edu>
2014-02-12 22:51:47 -0700

    Lesson 05 Paired Programming Assignment - Added GetMarkerView class that asks input of a valid marker for the user.

---
+++ src/connect4/GetMarkerView.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connect4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jesse
+ */
+public class GetMarkerView {
+    
+    public static void main(String[] args) {
+       GetMarkerView getMarker = new GetMarkerView();
+       getMarker.getInput();
+    }
+    
+    public String getInput() {
+
+        String newMarker = null;
+        Scanner in = new Scanner(System.in);
+                
+        boolean valid = false; 
+        while (!valid) {
+            
+            System.out.println("\n\tEnter a single character that will be "
+                    + "used to mark your squares in the game.");
+                            
+            newMarker = in.nextLine();
+           
+            if (newMarker == null  || newMarker.length() > 1) {
+                System.out.println ("\n\tInvalid number of characters or entry");
+                continue;
+            }
+            
+            newMarker = newMarker.substring(0, 1).toUpperCase();
+            valid = true;
+        }
+        
+        return newMarker;
+    }
+    
+}
+
