040f4eecf89c47b852e74bc96381cbfd2ff84a7c
Gneiting-Jesse <gne99003@byui.edu>
2014-02-22 22:47:17 -0700

    Lesson 06 Individual Assignment - Added SummationOfArrays class that calculates the summation of an array using the for each loop.

---
+++ src/connect4/SummationOfArrays.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package connect4;
+
+/**
+ *
+ * @author Jesse
+ */
+public class SummationOfArrays {
+    
+    public static void main(String[] args) {
+        double[] nums = {151.2, 43.3, 12.8};
+        double sum = 0;
+        for (double x : nums) {
+             sum += x;
+}
+        System.out.println("The summation of the array is "+sum);
+    }
+    
+}
